2b7c3c85c00d27b6c9e1761a919852a42cc79e28
Jon Janzen <jonjanzen@fb.com>
2020-10-21 15:44:09 -0700

    Fix hack and prepare for release (#120)
    Summary: Pull Request resolved: https://github.com/facebook/fbshipit/pull/120
    
    Test Plan: Hack on the GitHub PR
    
    Reviewed By: yns88
    
    Differential Revision: D24456486
    
    Pulled By: bigfootjon
    
    fbshipit-source-id: 06de847740523c05eb2bffeb3cf90bb146a65b4a

--- src/shipit/phase/ShipItCreateNewRepoPhase.php
+++ src/shipit/phase/ShipItCreateNewRepoPhase.php
@@ -12,6 +12,8 @@
  */
 namespace Facebook\ShipIt;
 
+use namespace HH\Lib\{C, Str, Vec};
+
 final class ShipItCreateNewRepoPhase extends ShipItPhase {
   private ?string $sourceCommit = null;
   private ?string $outputPath = null;
@@ -237,7 +239,8 @@ final class ShipItCreateNewRepoPhase extends ShipItPhase {
 
     $chunk_count = C\count($all_filenames_chunked);
 
-    C\fb\each_with_key($all_filenames_chunked, ($i, $chunk_filenames) ==> {
+    // @lint-ignore UNUSED_RESULT
+    Vec\map_with_key($all_filenames_chunked, ($i, $chunk_filenames) ==> {
       if ($manifest->isVerboseEnabled()) {
         $logger->out("    Processing chunk %d/%d", $i + 1, $chunk_count);
       }
