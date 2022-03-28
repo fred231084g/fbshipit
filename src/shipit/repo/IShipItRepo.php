fe82265df155efaea892b0395ef35cbbfae07483
Jon Janzen <jonjanzen@fb.com>
2022-03-28 12:26:58 -0700

    Fix copyright
    Reviewed By: wittgenst
    
    Differential Revision: D35188120
    
    fbshipit-source-id: 7f9490fbaf3da934bfd75297153bd74da15838a7

--- src/shipit/repo/IShipItRepo.php
+++ src/shipit/repo/IShipItRepo.php
@@ -1,5 +1,11 @@
 <?hh
-// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ * @format
+ */
 
 namespace Facebook\ShipIt;
 
