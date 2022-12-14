62c65c5569095571cc497be3d6716bf6e323d224
Jon Janzen <jonjanzen@meta.com>
2022-12-14 11:19:19 -0800

    use the trick from @yns88

--- tests/importit/SubmoduleTest.php
+++ tests/importit/SubmoduleTest.php
@@ -61,7 +61,7 @@ final class SubmoduleTest extends \Facebook\ShipIt\ShellTest {
 
   public async function testImportCommitPatchWithSubmodule(): Awaitable<void> {
     // This test does not work on GitHub Actions:
-    if (true) return; // @oss-enable
+    return static::markAsSkipped('broken on GitHub Actions'); // @oss-enable
     // First create a repo that we'll use as our submodule.
     $submodule_dir = new ShipItTempDir('submodule');
     await (new ShipItShellCommand($submodule_dir->getPath(), 'git', 'init'))
